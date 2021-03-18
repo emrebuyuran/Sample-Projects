c0d25f26c053978f42e986cb70c0262c3d0cec7e
Emre Buyuran <emre.byrn@gmail.com>
2021-03-18 04:48:28 +0300

    git init and files added

---
+++ BaseArithmetic/BaseArithmetic/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BaseArithmetic
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxBase = new System.Windows.Forms.ComboBox();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(831, 173);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 31);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Durdur";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(40, 63);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(496, 13);
+            this.lblInfo.TabIndex = 18;
+            this.lblInfo.Text = "*Bu uygulama 10\'luk tabanda yazılan iki basamaklı tamsayıları 2,3,4,5 ve 6\'lık ta" +
+    "banlara çevirmeye yarar. ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(444, 173);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Sonuç :";
+            // 
+            // tbxResult
+            // 
+            this.tbxResult.Location = new System.Drawing.Point(496, 170);
+            this.tbxResult.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxResult.Name = "tbxResult";
+            this.tbxResult.Size = new System.Drawing.Size(156, 20);
+            this.tbxResult.TabIndex = 16;
+            this.tbxResult.TextChanged += new System.EventHandler(this.tbxResult_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(831, 118);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 32);
+            this.btnCalculate.TabIndex = 15;
+            this.btnCalculate.Text = "Hesapla";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 213);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Çevrilecek Tabanı Seçiniz :";
+            // 
+            // cbxBase
+            // 
+            this.cbxBase.FormattingEnabled = true;
+            this.cbxBase.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.cbxBase.Location = new System.Drawing.Point(229, 209);
+            this.cbxBase.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxBase.Name = "cbxBase";
+            this.cbxBase.Size = new System.Drawing.Size(129, 21);
+            this.cbxBase.TabIndex = 13;
+            this.cbxBase.SelectedIndexChanged += new System.EventHandler(this.cbxBase_SelectedIndexChanged);
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Location = new System.Drawing.Point(227, 141);
+            this.tbxNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(132, 20);
+            this.tbxNumber.TabIndex = 12;
+            this.tbxNumber.TextChanged += new System.EventHandler(this.tbxNumber_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 145);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Çevrilecek Sayıyı Giriniz :";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 317);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxBase);
+            this.Controls.Add(this.tbxNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Taban Aritmetiği";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxBase;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BaseArithmetic/BaseArithmetic/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseArithmetic
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        int sayi, kalan;
+        string tutKalan = "";
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            tutKalan = "";
+            if (cbxBase.SelectedIndex == -1)
+            {
+                MessageBox.Show("Taban Seçiniz ", "Hata", MessageBoxButtons.OK);
+                return;
+            }
+
+
+            sayi = Convert.ToInt32(tbxNumber.Text);
+
+            if (sayi < 10 || sayi > 99)
+            {
+                MessageBox.Show("Girilen değer çok küçük veya bir tamsayı değil ! Lütfen 9'dan büyük, 99'dan küçük iki basamaklı bir sayı giriniz.", "Hata", MessageBoxButtons.OK);
+                return;
+            }
+            if (sayi >= 10 && sayi <= 99)
+            {
+
+                while (sayi != 0 && cbxBase.SelectedIndex == 0)
+                {
+                    kalan = sayi % 2;
+                    sayi /= 2;
+                    tutKalan = kalan.ToString() + tutKalan;
+                    tbxResult.Text = tutKalan;
+
+
+                }
+
+                while (sayi != 0 && cbxBase.SelectedIndex == 1)
+                {
+                    kalan = sayi % 3;
+                    sayi /= 3;
+                    tutKalan = kalan.ToString() + tutKalan;
+                    tbxResult.Text = tutKalan;
+
+
+
+                }
+                while (sayi != 0 && cbxBase.SelectedIndex == 2)
+                {
+                    kalan = sayi % 4;
+                    sayi /= 4;
+                    tutKalan = kalan.ToString() + tutKalan;
+                    tbxResult.Text = tutKalan;
+
+
+                }
+                while (sayi != 0 && cbxBase.SelectedIndex == 3)
+                {
+                    kalan = sayi % 5;
+                    sayi /= 5;
+                    tutKalan = kalan.ToString() + tutKalan;
+                    tbxResult.Text = tutKalan;
+
+
+                }
+                while (sayi != 0 && cbxBase.SelectedIndex == 4)
+                {
+                    kalan = sayi % 6;
+                    sayi /= 6;
+                    tutKalan = kalan.ToString() + tutKalan;
+                    tbxResult.Text = tutKalan;
+
+
+                }
+            }
+
+
+        }
+
+        private void tbxResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxBase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tbxNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
